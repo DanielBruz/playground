74f0de837ef6fa3f5a526cce65d5086faa7f6c3b
DanielBruz <daniel.bruzenak@tiscali.cz>
2023-07-14 22:21:05 +0200

    cviceni 9_2 (3)

--- cviceni_9_2.py
+++ cviceni_9_2.py
@@ -1,6 +1,6 @@
 # Uloha 2
-import turtle
 import random
+import turtle
 
 turtle.delay(10)
 t = turtle.Turtle()
@@ -11,65 +11,53 @@ def ctverec(strana):
     t.pencolor("#{:06x}".format(random.randrange(256 ** 3)))
     t.fillcolor("#{:06x}".format(random.randrange(256 ** 3)))
     t.begin_fill()
-    for i in range(4):
+    for i in range(6):  # vcetne 2 cyklu presunu pro start trojuhelniku
         t.lt(90)
         t.fd(strana)
     t.end_fill()
-    t.pu()
-    t.lt(90)
-    t.fd(strana)
-    t.pd()
+
 
 def trojuhelnik(strana):
     t.pensize(3)
     t.pencolor("#{:06x}".format(random.randrange(256 ** 3)))
     t.fillcolor("#{:06x}".format(random.randrange(256 ** 3)))
-    t.pu()
-    t.lt(90)
-    t.fd(strana)
-    t.pd()
     t.begin_fill()
     uhel = 120
     for i in range(3):
         t.rt(uhel)
         t.fd(strana)
     t.end_fill()
-
-def startovaci_pozice(strana, posun):
     t.pu()
     for i in range(2):
         t.lt(90)
         t.fd(strana)
-    t.fd(posun)
     t.pd()
+
 
 def domek(strana, posun):
     ctverec(strana)
     trojuhelnik(strana)
-    startovaci_pozice(strana, posun)
+    t.pu()
+    d = 15
+    t.fd(posun + d)
+    t.pd()
 
 def ulice():
     street = []
     sirka_strany_domu = -1
-    i = 0
-    j = 0
-    n = 0
-    posun = 0
+    i = n = 0
     while sirka_strany_domu != 0:
         sirka_strany_domu = int(input("Zadej sirku strany kazdeho domku (ukoncis '0'): "))
         if sirka_strany_domu != 0:
             street.append(sirka_strany_domu)
     n = int(len(street))
-    x = 20
-    for j in range(n):
-        if street[j] > street[j]:
-            posun = street[j] + x
-        else:
-            posun = street[j-1] + x
-        domek(street[j], posun)
+    for i in range(n):
+        if i < n - 1:
+            domek(street[i], street[i + 1])
+        elif i == n - 1:
+            domek(street[i], street[i])
+
 
 ulice()
 
 turtle.exitonclick()
-
-
